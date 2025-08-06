dcd118839c01fb4504f5f8f766724deb2ced8c3f
jacojanze <144199185+jacojanze@users.noreply.github.com>
2025-08-05 19:30:42 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,10 @@
-from flask import Flask, render_template_string, request, redirect, url_for
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    if request.method == "POST":
-        return redirect(url_for("dashboard"))
-    return render_template_string("""
-        <form method="post">
-            <input type="submit" value="Login">
-        </form>
-    """)
+@app.route("/")
+def hello():
+    return "Hello friends"
 
-@app.route("/dashboard")
-def dashboard():
-    return "Dashboard"
+if __name__ = "__main__":
+    app.run("0.0.0.0", 5000)
